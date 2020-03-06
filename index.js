80d64a0b8e22f04708185e5d1906a520ccfb9850
Thimo Reumerman <thimo.reumerman@gmail.com>
2020-03-06 12:02:01 +0100

    Added PORT env var

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}`));
 
 module.exports = server;
